c5f5573d1549a8d5d707e14f83c59a504ca60ec7
Leonardo <leonardo.oliver23@icloud.com>
2026-04-27 17:54:24 -0300

    Adicionando vercel

--- sales-app/vite.config.js
+++ sales-app/vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  // Isso garante que os caminhos dos arquivos no index.html 
+  // sejam gerados corretamente para o ambiente de produção
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
